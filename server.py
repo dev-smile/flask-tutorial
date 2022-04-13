eb0215934dbb5806d34392d983ca64585c17e546
JunHo Lee <kou2004@naver.com>
2022-04-14 01:35:12 +0900

    수정

--- server.py
+++ server.py
@@ -10,7 +10,12 @@ topics = [
     {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
 ]
 
-def template(contents, content):
+def template(contents, content, id=None):
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li><a href="/update/{id}/">update</a></li>
+        '''
     return f'''<!doctype html>
     <html>
         <body>
@@ -21,6 +26,7 @@ def template(contents, content):
             {content}
             <ul>
                 <li><a href="/create/">create</a></li>
+                {contextUI}
             </ul>
         </body>
     </html>
@@ -46,7 +52,7 @@ def read(id):
             title = topic['title']
             body = topic['body']
             break
-    return template(getContents(), f'<h2>{title}</h2>{body}')
+    return template(getContents(), f'<h2>{title}</h2>{body}', id)
 
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
@@ -67,6 +73,36 @@ def create():
         topics.append(newTopic)
         url = '/read/'+str(nextId)+'/'
         nextId = nextId + 1
+        return redirect(url)
+
+@app.route('/update/<int:id>/', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'GET': 
+        title = ''
+        body = ''
+        for topic in topics:
+            if id == topic['id']:
+                title = topic['title']
+                body = topic['body']
+                break
+        content = f'''
+            <form action="/update/{id}/" method="POST">
+                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
+                <p><textarea name="body" placeholder="body">{body}</textarea></p>
+                <p><input type="submit" value="update"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        for topic in topics:
+            if id == topic['id']:
+                topic['title'] = title
+                topic['body'] = body
+                break
+        url = '/read/'+str(id)+'/'
         return redirect(url)
 
 
