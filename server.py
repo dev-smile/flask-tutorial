d375bc1613b09cb5d6c261bd7c885db5134f5678
JunHo Lee <kou2004@naver.com>
2022-04-14 01:12:59 +0900

    읽기 구현, 중복 제거

--- server.py
+++ server.py
@@ -9,32 +9,45 @@ topics = [ # 실제로는 여기에 DB를 읽어오는 부분을 작성
     {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
 ]
 
-@app.route('/')
-def index(): # 함수 이름은 아무거나 해도 상관 없음
-    liTags = ''
-    for topic in topics:
-        liTags += f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
-
+def template(contents, content):
     return f'''<!doctype html>
     <html>
         <body>
             <h1><a href="/">WEB</a></h1>
             <ol>
-                {liTags}
+                {contents}
             </ol>
-            <h2>Welcome</h2>
-            Hello, Web
+            {content}
         </body>
     </html>
     '''
 
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+
+@app.route('/')
+def index(): # 함수 이름은 아무거나 해도 상관 없음
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/read/<int:id>/')
+def read(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+
+
 @app.route('/create/')
 def create():
     return 'Create'
-
-@app.route('/read/<id>/')
-def read(id):
-    return 'read '+id
 
 
 app.run(debug=True) # 실제 서버에서는 디버거 모드로 실행하면 안됨
