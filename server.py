45a612ea15201072569c63af67f724eda57bd836
JunHo Lee <kou2004@naver.com>
2022-04-14 00:54:43 +0900

    라우팅

--- server.py
+++ server.py
@@ -4,7 +4,16 @@ import random
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'random : <strong>' + str(random.random()) + '</strong>'
+def index(): # 함수 이름은 아무거나 해도 상관 없음
+    return 'welcome'
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+@app.route('/read/<id>')
+def read(id):
+    return 'read '+id
+
 
 app.run(debug=True) # 실제 서버에서는 디버거 모드로 실행하면 안됨
